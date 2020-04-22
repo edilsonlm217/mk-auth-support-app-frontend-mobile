f1270ed767e553a8ea5a4b7ae60342525f0702c8
Edilson Lima <edilson.lima@nextem.com.br>
2020-04-22 04:41:19 -0400

    #1 Criação do ícone de configurações no cabeçalho da aplicação

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function AppHeader(props) {
+export default function AppHeader( props ) {
+  function handleNavigateToSettings() {
+    props.navigation.navigate('Settings');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.header_title}>Chamados</Text>
+      <TouchableOpacity onPress={handleNavigateToSettings}>
+        <Icon style={styles.settings_icon} name="settings-outline" size={25} color="#FFF" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,6 +21,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#337AB7',
     height: '15%',
+    flexDirection: 'row',
+    justifyContent: "space-between",
   },
   header_title: {
     fontSize: 24,
@@ -20,5 +30,10 @@ const styles = StyleSheet.create({
     marginTop: 15,
     marginLeft: 20,
     color: '#FFF',
+  },
+
+  settings_icon: {
+    marginTop: 20,
+    marginRight: 20,
   },
 });
