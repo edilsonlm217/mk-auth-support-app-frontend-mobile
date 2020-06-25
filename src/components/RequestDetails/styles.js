78539cfe89d2a25b6c9125939895382d09d54d86
Edilson Lima <edilson.lima@nextem.com.br>
2020-06-25 01:02:21 -0400

    #1 Separação de estilo e lógica em arquivos diferentes

---
+++ src/components/RequestDetails/styles.js
@@ -0,0 +1,88 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFF",
+    position: "absolute",
+    bottom: 0,
+    width: '100%',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    padding: 20,
+  },
+  header_container: {
+    flexDirection: 'row',
+    marginBottom: 15,
+  },
+  
+  main_text: { 
+    fontWeight: "bold",
+    fontSize: 20,    
+  },
+  
+  sub_text: { 
+    fontSize: 16,
+    color: '#989898',
+  },
+  line_container: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  btns_contaier: {
+    flexDirection: "row",
+    justifyContent: 'space-around',
+    marginTop: 20,
+    backgroundColor: '#FFF'
+  },
+  secondary_btn: {
+    backgroundColor: '#FFF',
+    width: 160,
+    height: 45,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 6,
+  },
+  
+  secondary_btn_text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: '#337AB7',
+  },
+  
+  main_btn: {
+    backgroundColor: '#337AB7',
+    width: 160,
+    height: 45,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 6,
+  },
+
+  main_btn_text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: '#FFF',
+  },
+});
+
+export default styles;
