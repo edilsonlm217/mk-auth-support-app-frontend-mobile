8b615c0de1bd8e3dd19ff6f116ed311387544ffa
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-13 13:25:40 -0400

    #2 Refatoração da estrutura de rotas do sistema

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,11 @@ import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import UserIcon from 'react-native-vector-icons/AntDesign';
+
 import { store } from './store/store';
+import { icons } from './styles/index';
 
 import Home from './pages/Home/index';
 import Details from './pages/Details/index';
@@ -18,89 +22,28 @@ import { fonts } from './styles/index';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function AppStack() {
+function HomeTabs() {
   return (
-    <Stack.Navigator
-      screenOptions={{ gestureEnabled: true, headerShown: true }}
-    >
-      <Stack.Screen
+    <Tab.Navigator>
+      <Tab.Screen
         name="Chamados"
         component={Home}
-        options={{ headerShown: false }}
-      />
-
-      <Stack.Screen
-        name="Details"
-        component={Details}
         options={{
-          title: 'Mais detalhes',
-          headerStyle: {
-            backgroundColor: '#FFF',
-          },
-          headerTintColor: '#337AB7',
-          headerTransparent: true,
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: fonts.huge,
-            marginLeft: -20,
-          },
+          tabBarIcon: ({ color }) => (
+            <Icon name="home" size={icons.tiny} color={color} />
+          ),
         }}
       />
-
-      <Stack.Screen
-        name="CTOs"
-        component={CTOMapping}
+      <Tab.Screen
+        name="Clientes"
+        component={ClientsScreen}
         options={{
-          title: 'Mapa de CTOs',
-          headerStyle: {
-            backgroundColor: '#FFF',
-          },
-          headerTintColor: '#337AB7',
-          headerTransparent: true,
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: fonts.huge,
-            marginLeft: -20,
-          },
+          tabBarIcon: ({ color }) => (
+            <UserIcon name="user" size={icons.tiny} color={color} />
+          ),
         }}
       />
-
-      <Stack.Screen
-        name="UpdateClienteLocation"
-        component={PickNewLocation}
-        options={{
-          title: 'Atualizar endereço',
-          headerStyle: {
-            backgroundColor: '#FFF',
-          },
-          headerTintColor: '#337AB7',
-          headerTransparent: true,
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: fonts.huge,
-            marginLeft: -20,
-          },
-        }}
-      />
-
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          title: 'Configurações',
-          headerStyle: {
-            backgroundColor: '#FFF',
-          },
-          headerTintColor: '#337AB7',
-          headerTransparent: true,
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: fonts.huge,
-            marginLeft: -20,
-          },
-        }}
-      />
-    </Stack.Navigator>
+    </Tab.Navigator>
   );
 }
 
@@ -131,10 +74,87 @@ export default function RootTab() {
     <>
       {globalState.state.userToken !== null
         ?
-        <Tab.Navigator>
-          <Tab.Screen name="Chamados" component={AppStack} />
-          <Tab.Screen name="Clientes" component={ClientsScreen} />
-        </Tab.Navigator>
+        <Stack.Navigator
+          screenOptions={{ gestureEnabled: true, headerShown: true }}
+        >
+          <Stack.Screen
+            name="Chamados"
+            component={HomeTabs}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Details"
+            component={Details}
+            options={{
+              title: 'Mais detalhes',
+              headerStyle: {
+                backgroundColor: '#FFF',
+              },
+              headerTintColor: '#337AB7',
+              headerTransparent: true,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: fonts.huge,
+                marginLeft: -20,
+              },
+            }}
+          />
+
+          <Stack.Screen
+            name="CTOs"
+            component={CTOMapping}
+            options={{
+              title: 'Mapa de CTOs',
+              headerStyle: {
+                backgroundColor: '#FFF',
+              },
+              headerTintColor: '#337AB7',
+              headerTransparent: true,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: fonts.huge,
+                marginLeft: -20,
+              },
+            }}
+          />
+
+          <Stack.Screen
+            name="UpdateClienteLocation"
+            component={PickNewLocation}
+            options={{
+              title: 'Atualizar endereço',
+              headerStyle: {
+                backgroundColor: '#FFF',
+              },
+              headerTintColor: '#337AB7',
+              headerTransparent: true,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: fonts.huge,
+                marginLeft: -20,
+              },
+            }}
+          />
+
+          <Stack.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              title: 'Configurações',
+              headerStyle: {
+                backgroundColor: '#FFF',
+              },
+              headerTintColor: '#337AB7',
+              headerTransparent: true,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: fonts.huge,
+                marginLeft: -20,
+              },
+            }}
+          />
+        </Stack.Navigator>
         :
         <AuthStack />
       }
