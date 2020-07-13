2c17788a40b78f8c4077e2673b3bf1a83dbcfd8b
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-13 15:19:54 -0400

    #2 Implementação do layout base com listagem de clientes recuperados da API

--- src/pages/ClientsScreen/index.js
+++ src/pages/ClientsScreen/index.js
@@ -1,12 +1,110 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, Alert, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
-// import { Container } from './styles';
+import { store } from '../../store/store';
 
-export default function ClientsScreen() {
+import AppHeader from '../../components/AppHeader/index';
+
+export default function ClientsScreen({ navigation }) {
+  const globalState = useContext(store);
+
+  const [data, setData] = useState([]);
+
+  async function loadAPI() {
+    try {
+      const response = await axios.get(
+        `http://${globalState.state.server_ip}:${globalState.state.server_port}/clients`,
+        {
+          timeout: 4000,
+          headers: {
+            Authorization: `Bearer ${globalState.state.userToken}`,
+          },
+        },
+      );
+
+      setData(response.data);
+
+    } catch (error) {
+      Alert.alert('Erro', 'Erro ao chamar a API');
+    }
+  }
+
+  useEffect(() => {
+    loadAPI();
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Details!</Text>
+    <View style={styles.container}>
+      <AppHeader navigation={navigation} label="Clientes" altura="21%" />
+      <View style={styles.screen_container}>
+        <ScrollView>
+          {data.map(item => (
+            <>
+              <Text key={item.group} style={styles.group_label} >{item.group}</Text>
+              <View style={styles.card_style}>
+                <View>
+                  {item.children.map(client => (
+                    <TouchableOpacity key={client.id} style={styles.client_btn}>
+                      <Text numberOfLines={1}>{client.nome}</Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              </View>
+            </>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: '#337AB7',
+  },
+
+  screen_container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    backgroundColor: '#FFF',
+  },
+
+  card_style: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    padding: 15,
+    paddingTop: 0,
+    paddingBottom: 0,
+
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 5,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 10,
+  },
+
+  group_label: {
+    marginLeft: 20,
+    marginTop: 15,
+    marginBottom: 5
+  },
+
+  client_btn: {
+    borderBottomWidth: 0.5,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+});
