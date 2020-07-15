1f396cb2851f4888658336f5a11e4082d549b2cd
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-15 06:36:20 -0400

    #2 Implementação da view de busca

--- src/pages/SearchScreen/index.js
+++ src/pages/SearchScreen/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useContext, useReducer } from 'react';
+import { View, TextInput, Alert, RefreshControl, StyleSheet, TouchableOpacity, FlatList, ScrollView, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
-// import { Container } from './styles';
+import { store } from '../../store/store';
+import { icons, fonts } from '../../styles/index';
+import SearchIcon from 'react-native-vector-icons/Ionicons';
 
-export default function SearchScreen() {
+export default function ClientsScreen() {
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>);
+    <View style={styles.container}>
+      <View style={styles.search_area} >
+        <View style={{ paddingLeft: 10, paddingRight: 10 }}>
+          <SearchIcon name='search' size={18} color='#b0b0b0' />
+        </View>
+        <View style={{ width: '100%' }}>
+          <TextInput style={styles.input_style} placeholder="Ex: João Carlos" />
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#337AB7',
+  },
+
+  search_area: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    borderRadius: 20,
+    height: 35,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  input_style: {
+    fontSize: fonts.medium,
+    paddingTop: 2,
+    paddingBottom: 1,
+  },
+});
