a357249efc641ddaa761be294f785e810c092cc4
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-16 15:03:14 -0400

    #2 Implementação da Flatlist e states

--- src/pages/SearchScreen/index.js
+++ src/pages/SearchScreen/index.js
@@ -1,23 +1,41 @@
-import React, { useState, useEffect, useContext, useReducer } from 'react';
-import { View, TextInput, Alert, Text, StyleSheet, TouchableOpacity, FlatList, ScrollView, ActivityIndicator } from 'react-native';
-import axios from 'axios';
+import React, { useRef, useContext, useReducer } from 'react';
+import {
+  View,
+  TextInput,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  RefreshControl
+} from 'react-native';
 
+import { searchUtil } from '../../utils/search';
 import { store } from '../../store/store';
-import { icons, fonts } from '../../styles/index';
+import { fonts } from '../../styles/index';
 
 import SearchIcon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function ClientsScreen() {
   const globalState = useContext(store);
 
+  const refInput = useRef(null);
+
   const [state, dispatch] = useReducer(reducer, {
-    clients: null,
+    clients: [],
     loading: false,
     search_term: '',
   });
 
   function reducer(state, action) {
     switch (action.type) {
+      case 'clearState':
+        return {
+          ...state,
+          clients: [],
+          search_term: '',
+        }
+
       case 'setSearchResult':
         return {
           ...state,
@@ -58,22 +76,53 @@ export default function ClientsScreen() {
       type: 'loadingInit',
     });
 
-    const response = await axios.get(
-      `http://${globalState.state.server_ip}:${globalState.state.server_port}/search?term=${term}`
+    const response = await searchUtil(
+      `http://${globalState.state.server_ip}:${globalState.state.server_port}/search?term=${term}`,
+      {
+        timeout: 2500,
+        headers: {
+          Authorization: `Bearer ${globalState.state.userToken}`,
+        },
+      }
     );
 
-    let clients = [];
-    response.data.map(client => {
-      clients.push(client.nome)
-    });
+    if (response) {
+      dispatch({
+        type: 'setSearchResult',
+        payload: {
+          clients: response,
+        },
+      });
+    }
+  }
+
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity style={{ backgroundColor: '#FFF', padding: 5, margin: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text>{item}</Text>
+        <Icon name={'chevron-right'} size={20} color={'#000'} />
+      </TouchableOpacity>
+    );
+  }
+
+  function FlatListItemSeparator() {
+    return (
+      <View
+        style={{
+          height: 0.5,
+          width: "100%",
+          backgroundColor: "#000",
+        }}
+      />
+    );
+  }
+
+  function handleClearInputText() {
+    refInput.current.clear();
 
     dispatch({
-      type: 'setSearchResult',
-      payload: {
-        clients,
-      },
+      type: 'clearState'
     });
-
   }
 
   return (
@@ -82,16 +131,24 @@ export default function ClientsScreen() {
         <View style={{ paddingLeft: 10, paddingRight: 10 }}>
           <SearchIcon name='search' size={18} color='#b0b0b0' />
         </View>
-        <View style={{ width: '100%' }}>
-          <TextInput onChangeText={text => onChangeHandler(text)} style={styles.input_style} placeholder="Ex: João Carlos" />
+        <View style={{ width: '80%' }}>
+          <TextInput ref={refInput} onChangeText={text => onChangeHandler(text)} style={styles.input_style} placeholder="Ex: João Carlos" />
         </View>
+        <TouchableOpacity onPress={() => handleClearInputText()}>
+          <Icon name="close" size={18} color={'#b0b0b0'} />
+        </TouchableOpacity>
       </View>
-      {state.clients &&
-        state.clients.map(name => (
-          <Text>{name}</Text>
-        ))
-
-      }
+      <View style={{ backgroundColor: '#FFF', margin: 20, borderRadius: 6 }}>
+        <FlatList
+          data={state.clients}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          ItemSeparatorComponent={FlatListItemSeparator}
+          refreshControl={
+            <RefreshControl refreshing={state.loading} />
+          }
+        />
+      </View>
     </View>
   );
 }
