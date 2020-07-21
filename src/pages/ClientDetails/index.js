749f25cdb517654a51129c61a585a0efa81d6f61
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-20 21:04:20 -0400

    #3 Implementação de lógica para atualizaçaõ da coordenada do cliente

--- src/pages/ClientDetails/index.js
+++ src/pages/ClientDetails/index.js
@@ -18,6 +18,7 @@ import LocationServicesDialogBox from "react-native-android-location-services-di
 import openMap from 'react-native-open-maps';
 import AppHeader from '../../components/AppHeader/index';
 import axios from 'axios';
+import Modal from 'react-native-modal';
 
 import { icons, fonts } from '../../styles/index';
 import { store } from '../../store/store';
@@ -28,8 +29,10 @@ export default function ClientDetails({ navigation, route }) {
   const globalState = useContext(store);
 
   const [client, setClient] = useState({});
+
   const [refreshing, setRefreshing] = useState(false);
 
+  const [isVisible, setIsVisible] = useState(false);
 
   async function loadAPI() {
     try {
@@ -44,6 +47,8 @@ export default function ClientDetails({ navigation, route }) {
           },
         },
       );
+
+      console.log(response.data);
 
       setClient(response.data);
       setRefreshing(false);
@@ -67,7 +72,7 @@ export default function ClientDetails({ navigation, route }) {
       preventBackClick: false,
       providerListener: false
     }).then(function (success) {
-      OpenCoordinate(client.coordenadas);
+      setIsVisible(true);
     }).catch((error) => {
       // console.log(error.message);
     });
@@ -123,6 +128,53 @@ export default function ClientDetails({ navigation, route }) {
 
     Linking.openURL(phoneNumber);
   };
+
+  function handleNavigateNewLocationPicker() {
+    setIsVisible(false);
+    navigation.navigate('UpdateClienteLocation', {
+      data: {
+        id: client.client_id,
+      }
+    });
+  }
+
+  function handleModalClosing() {
+    setIsVisible(false);
+  }
+
+  async function OpenCoordinate(coordinate) {
+    const [latidude, longitude] = coordinate.split(',');
+
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        const { } = Geolocation.getCurrentPosition(geo_success => {
+          const current_longitude = geo_success.coords.longitude;
+          const current_latitude = geo_success.coords.latitude;
+
+          openMap({
+            provider: 'google',
+            start: `${current_latitude},${current_longitude}`,
+            end: `${latidude},${longitude}`
+          });
+        }, geo_error => {
+          console.log(geo_error)
+          Alert.alert('Erro', 'Não é possível navegar até o cliente');
+        }, {
+          timeout: 5000,
+          enableHighAccuracy: true,
+        });
+      } else {
+        Alert.alert('Erro', 'Não foi possível recuperar sua Localização');
+      }
+    } catch (err) {
+      console.log(err);
+      Alert.alert('Erro', 'Não é possível navegar até o cliente');
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -197,6 +249,27 @@ export default function ClientDetails({ navigation, route }) {
         </TouchableOpacity>
 
       </ScrollView>
+
+      <Modal
+        onBackButtonPress={handleModalClosing}
+        onBackdropPress={handleModalClosing}
+        children={
+          <View style={styles.modal_style}>
+            <Text style={styles.modal_header}>Selecione uma opção...</Text>
+            <TouchableOpacity onPress={() => OpenCoordinate(client.coordenadas)} style={styles.modal_btn}>
+              <Text style={styles.modal_btn_style}>Navegar até cliente</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleNavigateNewLocationPicker(client.coordenadas)} style={styles.modal_btn}>
+              <Text style={styles.modal_btn_style}>Atualizar coordenadas</Text>
+            </TouchableOpacity>
+          </View>
+        }
+        isVisible={isVisible}
+        style={{ margin: 0 }}
+        animationInTiming={500}
+        animationOutTiming={500}
+        useNativeDriver={true}
+      />
     </View>
   );
 }
@@ -233,5 +306,51 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
     flexDirection: 'row',
     justifyContent: "space-between"
+  },
+
+  modal_style: {
+    width: '100%',
+    maxWidth: 275,
+    backgroundColor: "#FFF",
+    alignSelf: "center",
+    borderWidth: 0,
+    borderRadius: 10,
+    padding: 20,
+    paddingTop: 10,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+
+  modal_header: {
+    fontWeight: "bold",
+    fontSize: fonts.medium,
+    width: '100%',
+    marginBottom: 10,
+  },
+
+  modal_btn: {
+    width: '100%',
+    height: 35,
+    marginTop: 15,
+    display: "flex",
+    justifyContent: 'center',
+    borderRadius: 4,
+    backgroundColor: '#FFF',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 4,
+  },
+
+  modal_btn_style: {
+    fontSize: fonts.medium,
+    paddingLeft: 15,
+    textAlign: "center",
   },
 });
