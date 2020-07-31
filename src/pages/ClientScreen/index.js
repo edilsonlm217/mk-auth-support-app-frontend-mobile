2130795635bed6c8e3b0ddf102cb28fc6161f6fd
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-31 14:41:18 -0400

    #3 Ajuste de tamanho de fonte

--- src/pages/ClientScreen/index.js
+++ src/pages/ClientScreen/index.js
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
   label_style: {
     color: '#FFF',
     fontWeight: 'bold',
-    fontSize: fonts.medium,
+    fontSize: fonts.regular,
     alignSelf: 'center',
   },
 
