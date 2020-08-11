8d56aea37551960b8de41792c56fed7a297478a6
Edilson Lima <edilsonlm217@hotmail.com>
2020-08-11 11:25:53 -0400

    #5 Implementação completa do accordeon de faturas pagas

--- src/pages/ClientFinancing/index.js
+++ src/pages/ClientFinancing/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Switch, StyleSheet } from 'react-native';
+import { View, Text, Switch, StyleSheet, ScrollView } from 'react-native';
 import Accordion from 'react-native-collapsible/Accordion';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -24,23 +24,58 @@ export default function ClientFinancing() {
       title: 'Second',
       content: 'Lorem ipsum...',
     },
+    {
+      title: 'Second',
+      content: 'Lorem ipsum...',
+    },
   ];
 
-  const _renderHeader = section => {
+  const _renderHeader = (section) => {
     return (
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <Icon name="chevron-down-circle" size={icons.large} color="#337AB7" />
-        <Text style={[styles.main_text, { fontSize: fonts.medium, width: '55%', textAlign: 'center' }]}>Vencimento</Text>
-        <Text style={{ fontSize: fonts.medium }}>14/06/2020</Text>
+        <View style={{ width: '55%', alignItems: 'center' }}>
+          <Text style={[styles.main_text, { fontSize: fonts.medium, textAlign: 'left', width: '50%' }]}>Vencimento</Text>
+        </View>
+        <Text>14/06/2020</Text>
       </View>
     );
   };
 
   const _renderContent = section => {
     return (
-      <View>
-        <Text>{section.content}</Text>
-      </View>
+      <>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Icon name="chevron-down-circle" size={icons.large} color="#FFF" />
+          <View style={{ width: '55%', alignItems: 'center' }}>
+            <Text style={[styles.main_text, { fontSize: fonts.medium, textAlign: 'left', width: '50%' }]}>Tipo</Text>
+          </View>
+          <Text>Mensalidade</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Icon name="chevron-down-circle" size={icons.large} color="#FFF" />
+          <View style={{ width: '55%', alignItems: 'center' }}>
+            <Text style={[styles.main_text, { fontSize: fonts.medium, textAlign: 'left', width: '50%' }]}>Valor</Text>
+          </View>
+          <Text>R$ 120,00</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Icon name="chevron-down-circle" size={icons.large} color="#FFF" />
+          <View style={{ width: '55%', alignItems: 'center' }}>
+            <Text style={[styles.main_text, { fontSize: fonts.medium, textAlign: 'left', width: '50%' }]}>Status</Text>
+          </View>
+          <Text>À vencer</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Icon name="chevron-down-circle" size={icons.large} color="#FFF" />
+          <View style={{ width: '55%', alignItems: 'center' }}>
+            <Text style={[styles.main_text, { fontSize: fonts.medium, textAlign: 'left', flex: 1, width: '50%' }]}>Descrição</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+          </View>
+        </View>
+      </>
     );
   };
 
@@ -49,11 +84,13 @@ export default function ClientFinancing() {
   };
 
   return (
-    <View>
+    <ScrollView>
       <View style={styles.observation_section}>
         <View style={{ flex: 1 }}>
           <Text style={styles.main_text}>Em observação</Text>
-          <Text style={styles.sub_text}>Habilitar o modo observasão impedirá que o sistema bloqueie o cliente</Text>
+          <Text style={styles.sub_text}>
+            Habilitar o modo observasão impedirá que o sistema bloqueie o cliente
+          </Text>
         </View>
         <Switch
           trackColor={{ false: "#767577", true: "#81b0ff" }}
@@ -79,14 +116,10 @@ export default function ClientFinancing() {
       </View>
 
       <View style={styles.invoices}>
-        <Text style={styles.main_text}>Faturas em aberto</Text>
-        <View style={{ flexDirection: 'row', marginTop: 10, alignItems: 'center' }}>
-          <Icon name="chevron-down-circle" size={icons.large} color="#337AB7" />
-          <Text style={[styles.main_text, { fontSize: fonts.medium, width: '55%', textAlign: 'center' }]}>Vencimento</Text>
-          <Text>14/06/2020</Text>
-        </View>
+        <Text style={[styles.main_text, { marginBottom: 10 }]}>Faturas pagas</Text>
+
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
