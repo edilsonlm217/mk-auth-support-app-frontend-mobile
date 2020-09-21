7df086d7fac2f23d5e6acc9275f6ec6e1eecb4e0
Edilson Lima <edilsonlm217@hotmail.com>
2020-09-21 08:53:30 -0400

    #7 Implementação da header

--- src/pages/SearchScreen/index.js
+++ src/pages/SearchScreen/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { fonts } from '../../styles/index';
 
 export default function SearchScreen() {
   return (
-    <View>
-      <Text>Search Screen</Text>
-    </View>
+    <>
+      <View style={[styles.container, { height: '21%' }]}>
+        <View style={styles.header_container}>
+          <Text numberOfLines={1} style={styles.header_title}>
+            Buscar clientes
+          </Text>
+        </View>
+        <Text>Search Screen</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#337AB7',
+  },
+
+  header_title: {
+    fontSize: fonts.huge,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+
+  header_container: {
+    marginTop: 15,
+    marginLeft: 15,
+    justifyContent: "center",
+    height: '30%',
+  },
+});
