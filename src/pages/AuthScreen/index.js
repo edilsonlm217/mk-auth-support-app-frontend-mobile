db374842b28e673cc74aee36c5259a7c5df96584
Edilson Lima <edilsonlm217@hotmail.com>
2020-10-13 13:25:19 -0400

    #7 Construção do novo layout de tela

--- src/pages/AuthScreen/index.js
+++ src/pages/AuthScreen/index.js
@@ -4,6 +4,7 @@ import {
   Text,
   Image,
   TextInput,
+  Switch,
   TouchableOpacity,
   Alert, ActivityIndicator
 } from 'react-native';
@@ -14,12 +15,11 @@ import Modal from 'react-native-modal';
 
 import { store } from '../../store/store';
 
-import lock from '../../assets/unlocked.png';
+import app_logo from '../../assets/mk-edge-logo.png';
 
-import styles from './styles';
 import { icons } from '../../styles/index';
 
-export default function AuthScreen({ route, navigation }) {
+export default function AuthScreen({ route }) {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
@@ -49,93 +49,178 @@ export default function AuthScreen({ route, navigation }) {
 
   }
 
-  function handlePrevScreen() {
-    navigation.goBack();
-  }
-
   return (
     <View style={{ flex: 1 }}>
       <LinearGradient
-        colors={['#002f58', '#337ab7']}
-        style={styles.initial_config_linearGradient}
+        colors={['#002F58', '#001C34']}
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
       >
-        <View style={styles.initial_config_container}>
-          <Image source={lock} style={styles.initial_config_logo_style} />
+        <View style={{
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: 20,
+        }}
+        >
+          <Image
+            source={app_logo}
+            style={{
+              width: 120,
+              height: 120,
+              borderRadius: 60,
+              borderWidth: 3,
+              borderColor: '#D5D5D5',
+              marginBottom: 45,
+              marginTop: 10,
+            }}
+          />
 
-          <Text style={styles.initial_config_main_title} >Quase lá</Text>
-
-          <HideWithKeyboard>
-            <Text style={styles.initial_config_sub_title} >
-              Agora informe seu usuário e senha para carregar chamados
-          </Text>
-          </HideWithKeyboard>
-
-          <View style={[styles.initial_config_input_container]}>
-            <View style={styles.initial_config_icon_container}>
-              <Icon name="account" size={icons.small} color="#002f58" />
-            </View>
+          <View style={{
+            backgroundColor: '#EAEAEA',
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderRadius: 5,
+            paddingLeft: 10,
+            height: 45,
+          }}
+          >
+            <Icon name="key" size={icons.tiny} color="#555555" />
             <TextInput
               autoCorrect={false}
               autoCapitalize="none"
-              placeholder="Login do técnico"
-              style={styles.initial_config_text_input_style}
-              onChangeText={login => setLogin(login)}
+              placeholder="Chave de acesso"
+              placeholderTextColor='#555555'
+              style={{
+                fontSize: 16,
+                flex: 1,
+                fontFamily: "Roboto-Light",
+                paddingLeft: 15,
+                padding: 0,
+                height: '100%',
+              }}
             />
           </View>
 
-          <View style={[styles.initial_config_input_container]}>
-            <View style={styles.initial_config_icon_container}>
-              <Icon name="lock" size={icons.small} color="#002f58" />
-            </View>
+          <View style={{
+            marginBottom: 5,
+            marginTop: 5,
+            flexDirection: 'row',
+            alignItems: 'center',
+            alignSelf: 'flex-end',
+          }}>
+            <Text
+              style={{
+                fontFamily: "Roboto-Light",
+                fontSize: 12,
+                color: '#EAEAEA',
+              }}
+            >Nunca esquecer</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={true ? "#f4f3f4" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              value={true}
+              style={{ transform: [{ scaleX: .8 }, { scaleY: .8 }] }}
+            />
+          </View>
+
+          <View style={{
+            backgroundColor: '#EAEAEA',
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderRadius: 5,
+            paddingLeft: 10,
+            height: 45,
+          }}
+          >
+            <Icon name="account" size={icons.tiny} color="#555555" />
             <TextInput
-              placeholder="Sua senha secreta"
-              style={styles.initial_config_text_input_style}
-              onChangeText={password => setPassword(password)}
-              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Login"
+              placeholderTextColor='#555555'
+              style={{
+                fontSize: 16,
+                flex: 1,
+                fontFamily: "Roboto-Light",
+                paddingLeft: 15,
+                padding: 0,
+                height: '100%',
+              }}
             />
           </View>
-        </View>
 
-        <TouchableOpacity
-          onPress={handlePrevScreen}
-          style={styles.initial_config_prev_btn_style}
-        >
-          <HideWithKeyboard style={{ flexDirection: 'row' }}>
-            <Icon name="chevron-left" size={icons.small} color="#FFF" />
-            <Text style={styles.initial_config_navigators_text_style}>
-              Voltar
-          </Text>
-          </HideWithKeyboard>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={handleSignIn}
-          style={styles.initial_config_next_btn_style}
-        >
-          <HideWithKeyboard style={{ flexDirection: 'row' }}>
-            <Text style={styles.initial_config_navigators_text_style}>
-              Conectar
-            </Text>
-            <Icon name="chevron-right" size={icons.small} color="#FFF" />
-          </HideWithKeyboard>
-        </TouchableOpacity>
-      </LinearGradient>
-
-      <Modal
-        children={
-          <View style={styles.modal_style}>
-            <ActivityIndicator size="small" color="#0000ff" />
-            <Text style={styles.modal_text_style}>
-              Carregando...
-            </Text>
+          <View style={{
+            backgroundColor: '#EAEAEA',
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderRadius: 5,
+            paddingLeft: 10,
+            height: 45,
+            marginTop: 10,
+          }}
+          >
+            <Icon name="lock" size={icons.tiny} color="#555555" />
+            <TextInput
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Sua senha secreta"
+              placeholderTextColor='#555555'
+              style={{
+                fontSize: 16,
+                flex: 1,
+                fontFamily: "Roboto-Light",
+                paddingLeft: 15,
+                padding: 0,
+                height: '100%',
+              }}
+            />
           </View>
-        }
-        isVisible={isVisible}
-        style={{ margin: 0 }}
-        animationInTiming={500}
-        animationOutTiming={500}
-        useNativeDriver={true}
-      />
+
+          <TouchableOpacity
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              backgroundColor: '#4B60C7',
+              borderRadius: 5,
+              height: 45,
+              marginTop: 80,
+              width: '100%',
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 16,
+                  marginLeft: 10,
+                  fontFamily: "Roboto-Medium",
+                  color: '#FFFFFF',
+                  padding: 0,
+                }}
+              >Autenticar</Text>
+              <Icon name="login" size={icons.tiny} color="#FFF" style={{ marginLeft: 10 }} />
+            </View>
+          </TouchableOpacity>
+
+          <HideWithKeyboard>
+            <TouchableOpacity style={{ flexDirection: 'row', marginTop: 50, }}>
+              <Text style={{ color: '#FFF' }}>Sem chave de acesso?</Text>
+              <Text style={{ fontFamily: 'Roboto-Bold', color: '#FFF' }}> Registrar.</Text>
+            </TouchableOpacity>
+          </HideWithKeyboard>
+
+        </View>
+      </LinearGradient>
     </View>
   );
 }
