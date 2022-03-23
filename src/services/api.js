9fb7659c58121d153f9c6e2b9e24527263f48f4d
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-03-23 19:53:56 -0300

    Configuração de base url da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.3.69:3333', // when localhost
-  // baseURL: 'http://mk-edge.com.br:3333', // when preprod and prod
+  // baseURL: 'http://192.168.3.69:3333', // when localhost
+  baseURL: 'http://mk-edge.com.br:3335', // when preprod and prod
 });
 
 export default api;
